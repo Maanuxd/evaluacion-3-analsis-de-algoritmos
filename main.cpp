c6d91c01c45f70044d42bf7f79e841b74561c16f
Ketrafe <56241911+Ketrafe@users.noreply.github.com>
2020-08-07 21:20:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 int main(int argc, char **argv){
-    
+    /*
+    ** comprobador de lectura de argumentos por consola al momento de ejecutar
+    cout<<"\tse ha operado el programa con " <<argc<<" argumentos \n";
+    for (int i = 0; i< argc ; ++i){
+        cout << "\t\targumento N: "<<i<<" = "<< argv[i] << "\n";
+    }
+    **
+    */
+
+   return 0;
 }
